9327be0552b08114ba59c0dd3443f83696551d11
Michal Maciejewski <michal.m.pl@gmail.com>
2013-09-29 15:07:34 +0200

    Add test for empty matrix creation.

--- tests/tests.js
+++ tests/tests.js
@@ -3,3 +3,16 @@ test( "hello test", function() {
   ok( 1 == "1", "Passed!" );
 });
 */
+
+var matrix = new Matrix(2, 3);
+
+test("empty matrix creation", function() {
+  deepEqual(
+    matrix.content,
+    [
+      [0, 0, 0],
+      [0, 0, 0],
+    ]
+  );
+});
+
