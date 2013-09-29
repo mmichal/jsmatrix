41bdc719c1b3b585a3b63f0e9400c4b12b4680cb
Michal Maciejewski <michal.m.pl@gmail.com>
2013-09-29 15:25:00 +0200

    Add assertions for different matrix sizes in empty matrix test.

--- tests/tests.js
+++ tests/tests.js
@@ -11,8 +11,31 @@ test("empty matrix creation", function() {
     matrix.content,
     [
       [0, 0, 0],
-      [0, 0, 0],
+      [0, 0, 0]
     ]
   );
+
+  matrix = new Matrix(3, 2)
+  deepEqual(
+    matrix.content,
+    [
+      [0, 0],
+      [0, 0],
+      [0, 0]
+    ]
+  );
+
+  matrix = new Matrix(4, 4)
+  deepEqual(
+    matrix.content,
+    [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0]
+    ]
+  );
+
+
 });
 
